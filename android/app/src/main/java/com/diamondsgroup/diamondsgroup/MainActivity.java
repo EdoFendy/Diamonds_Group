be61ba737598a6c0e35047bfb9e9003c9a9d13d6
EdoFendy <128652373+EdoFendy@users.noreply.github.com>
2024-12-10 19:25:45 +0100

    Android e ios

---
+++ android/app/src/main/java/com/diamondsgroup/diamondsgroup/MainActivity.java
@@ -0,0 +1,5 @@
+package com.diamondsgroup.diamondsgroup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.diamondsgroup.diamondsgroup',
+  appName: 'diamondsgroup',
+  webDir: 'dist',
+  bundledWebRuntime: false
+ 
+};
+
+export default config;
