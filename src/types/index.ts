040558108f664d16216bd5674efb6f78af068b37
EdoFendy <128652373+EdoFendy@users.noreply.github.com>
2024-12-11 16:35:18 +0100

    aggiunto pwa

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
+import { Timestamp } from "firebase/firestore";
 import { ReactNode } from "react";
 
-// types.ts
 export type Ruolo = "Base" | "Avanzato" | "admin" | "utente";
 
 export interface User {
@@ -10,11 +10,23 @@ export interface User {
   cognome: string;
   email: string;
   codiceUnivoco: string;
-  referral: string;
   ruolo: Ruolo;
   dataRegistrazione: Date;
-  sponsor: string;
+  sponsor?: string; // Sponsor è ora opzionale
 }
+
+export interface Lead {
+  id: string;
+  nome: string;
+  cognome: string;
+  email: string;
+  telefono: string;
+  createdAt: Timestamp;
+  contattato?: boolean;
+  commenti?: string;
+  sponsor?: string; // Sponsor è opzionale
+}
+
 
 // types.ts
 export interface ZoomMeeting {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Diamonds Group',
+        short_name: 'Diamonds',
+        description: 'Diamonds Group Official App',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
